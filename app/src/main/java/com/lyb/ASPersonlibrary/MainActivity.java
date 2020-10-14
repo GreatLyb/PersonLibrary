fd5f8eebb87042200179d7f88867b155f03cf6d0
Lyb1005 <1778934152@qq.com>
2020-10-14 16:30:04 +0800

    Initial commit

---
+++ app/src/main/java/com/lyb/ASPersonlibrary/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lyb.ASPersonlibrary;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
